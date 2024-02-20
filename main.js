e8db3c4fd41a29fe9cf29b558ba8034e81889717
Richardpwe <112416359+Richardpwe@users.noreply.github.com>
2024-02-20 20:11:15 +0100

    Player

---
+++ main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDevPromise = import('electron-is-dev');
+
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+
+    const startURL = isDevPromise
+        ? 'http://localhost:3000'
+        : `file://${path.join(__dirname, '../build/index.html')}`;
+
+    mainWindow.loadURL(startURL);
+
+    mainWindow.on('closed', () => (mainWindow = null));
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
