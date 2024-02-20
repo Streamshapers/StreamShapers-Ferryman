3a43c61c748082b2d06fa8b92613e6919207255f
Richardpwe <112416359+Richardpwe@users.noreply.github.com>
2024-02-20 21:53:08 +0100

    tested electron

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ let mainWindow;
 
 function createWindow() {
     mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1400,
+        height: 1000,
         webPreferences: {
             nodeIntegration: true,
         },
