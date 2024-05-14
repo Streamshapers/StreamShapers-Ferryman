2f39ef1279622bbe603b65d360b6cda2a9fdc110
Richardpwe <112416359+Richardpwe@users.noreply.github.com>
2024-05-14 14:06:34 +0200

    v1.5.1.0 performance update

--- src/Theme/ThemeSwitch.js
+++ src/Theme/ThemeSwitch.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {useTheme} from './ThemeContext';
+import {faMoon, faSun} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const ThemeSwitch = () => {
     const {theme, toggleTheme} = useTheme();
 
     return (<>
             <div className="headerButton headerButton1 theme-switch" onClick={toggleTheme}>
-                <div><i className={`fas ${theme === 'dark' ? 'fa-moon' : 'fa-sun'}`}></i></div>
+                {theme === 'dark' && (<FontAwesomeIcon icon={faMoon} title="Export"/>)}
+                {theme === 'light' && (<FontAwesomeIcon icon={faSun} title="Export"/>)}
             </div>
         </>
     );
