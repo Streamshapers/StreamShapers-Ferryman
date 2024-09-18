54a53ff8478c9caaeeefb0a97bb6f9f4bbcd4afb
Richardpwe <112416359+Richardpwe@users.noreply.github.com>
2024-09-19 00:49:28 +0200

    samples

--- src/GlobalStateContext.js
+++ src/GlobalStateContext.js
@@ -3,7 +3,7 @@ import React, {createContext, useEffect, useRef, useState} from 'react';
 export const GlobalStateContext = createContext();
 
 export const GlobalStateProvider = ({children}) => {
-    const [ferrymanVersion] = useState("v1.6.0");
+    const [ferrymanVersion] = useState("v1.6.1");
     const [error, setError] = useState(null);
     const [jsonData, setJsonData] = useState(null);
     const [colors, setColors] = useState([]);
