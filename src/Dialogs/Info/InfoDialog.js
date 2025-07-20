bd44ac7373cd2dd190fe92925ae91a156d185364
jdoppelp <JP.Peters@gmx.de>
2025-07-20 21:01:47 +0200

    v2.0.0 Update Info Box

--- src/Dialogs/Info/InfoDialog.js
+++ src/Dialogs/Info/InfoDialog.js
@@ -5,7 +5,9 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 function InfoDialog() {
     const {theme, ferrymanVersion} = useContext(GlobalStateContext);
-    const [isImprintOpen, setIsImprintOpen] = useState(false);
+    const [isInfoOpen, setIsInfoOpen] = useState(false);
+    const [isContactOpen, setIsContactOpen] = useState(false);
+    const [isLicenseOpen, setIsLicenseOpen] = useState(false);
 
     return (<>
             <div id="info-title">
@@ -18,21 +20,43 @@ function InfoDialog() {
 
                 <div className="accordion-wrapper">
                     <div className="accordion">
-                        <div className="accordion-item imprint">
-                            <h3 className="accordion-header" onClick={() => setIsImprintOpen(!isImprintOpen)}>
-                                <FontAwesomeIcon icon={isImprintOpen ? faChevronUp : faChevronDown}/> Imprint
+                        <div className="accordion-item infos">
+                            <h3 className={`accordion-header ${isInfoOpen ? 'open' : ''}`} onClick={() => setIsInfoOpen(!isInfoOpen)}>
+                                <FontAwesomeIcon icon={isInfoOpen ? faChevronUp : faChevronDown}/> Infos
                             </h3>
-                            <div className={`accordion-body ${isImprintOpen ? 'open' : ''}`}>
-                                <p>StreamShapers by Jan-Philipp Peters, Nico Peters and Richard Weyer</p>
+                            <div className={`accordion-body ${isInfoOpen ? 'open' : ''}`}>
+                                <p>StreamShapers Ferryman created by Jan-Philipp Peters, Nico Peters and Richard Weyer</p>
+                                <p> Read <a href="https://www.streamshapers.com/imprint">Imprint</a> and <a href="https://www.streamshapers.com/privacy-policy">Privacy Policy</a></p>
+                            </div>
+                        </div>
+                        <div className="accordion-item License">
+                            <h3 className={`accordion-header ${isLicenseOpen ? 'open' : ''}`} onClick={() => setIsLicenseOpen(!isLicenseOpen)}>
+                                <FontAwesomeIcon icon={isLicenseOpen ? faChevronUp : faChevronDown}/> License
+                            </h3>
+                            <div className={`accordion-body ${isLicenseOpen ? 'open' : ''}`}>
+                                <p> <b>Streamshapers Ferryman </b> is licensed under the GNU General Public License v3.0 (GPL-3.0).<br></br>
+                                For more details, see the <a href="https://github.com/Streamshapers/StreamShapers-Ferryman/blob/master/LICENSE.txt">LICENSE</a> file.<br></br>
+                                By contributing to this project, you agree to abide by its terms.
+                                <br></br><br></br>
+                                The included <b>Lottie Player</b> is provided under MIT <a href="https://github.com/LottieFiles/lottie-player/blob/master/LICENSE">LICENSE</a>
+                                </p>
+                            </div>
+                        </div>
+                        <div className="accordion-item Contact">
+                            <h3 className={`accordion-header ${isContactOpen ? 'open' : ''}`} onClick={() => setIsContactOpen(!isContactOpen)}>
+                                <FontAwesomeIcon icon={isContactOpen ? faChevronUp : faChevronDown}/> Contact
+                            </h3>
+                            <div className={`accordion-body ${isContactOpen ? 'open' : ''}`}>
                                 <p>E-Mail: <a href="mailto:mail@streamshapers.com">mail@streamshapers.com</a></p>
                             </div>
                         </div>
                     </div>
                 </div>
 
+
             <div id="info-copyright">
                 <div>
-                    © StreamShapers 2024
+                    © StreamShapers 2025
                 </div>
             </div>
             <div className="popupButtonArea">
